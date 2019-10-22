2af1aaef2353e42183bfea7a05684cb695294de9
npj6 <32128571+npj6@users.noreply.github.com>
2019-10-22 23:41:57 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ printf "Cities\tGas\tDist\tWeight\tTime (ms)\n"
 input="sizes.test"
 while IFS= read -r line
 do
-  if [ -n "$VAR" ]; then
+  if [ -n "$line" ]; then
     ./data_gen.exe problems/index.p $line -t
     ./opt.exe "results.sol" problems/index.p -t
     make cleanProblems >> results.sol
